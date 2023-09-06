58985455e54179d238cad2985207fe6249c97193
7871017491 <144202055+7871017491@users.noreply.github.com>
2023-09-06 10:33:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#write the program that determine wheather a year enter by the user not usung if else statement
+number=int(input())
+if number%4==0:
+  print("leap year")
+else:
+  print("not leap year")
